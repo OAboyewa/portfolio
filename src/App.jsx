755a0a544fb87471b3181bc9dcd2873d7bf5ae21
OAboyewa <oluwatobiaboyewa@gmail.com>
2023-10-05 23:18:26 +0100

    refactor homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css'
 import Home from '/src/pages/Home.jsx';
 import Project from '/src/components/Project.jsx';
+import Contact from '/src/forms/Contact';
 
 function App() {
   const [distance, setDistance] = useState(0);
@@ -30,12 +31,26 @@ function App() {
           <span className="h1 ft-primary">Hello, I am Tobi Aboyewa <br /> a Front-End Developer <br /> based in Bedfordshire, UK</span>
         </div>
       </div>
+      <div>
+        <div className="h2 ft-primary cnt-align">
+          <span>SKILLS</span>
+        </div>
+        <div className="row">
+          <div className="column badges-cnt">
+              <img className="badges-img" src="\src\images\badges\React-Icon.png" alt="React" />
+              <img className="badges-img" src="\src\images\badges\Git-Icon-1788C.png" alt="Git" />
+              <img className="badges-img" src="\src\images\badges\github-mark.png" alt="Github" />
+              <img className="badges-img" src="\src\images\badges\Vite-Icon.png" alt="Vite" />
+              <img className="badges-img" src="\src\images\badges\vscode-icon.png" alt="VSCode" />
+          </div>
+        </div>
+      </div>
       <div className="stn-projects">
         <div className="h2 ft-primary cnt-align">
           <span>PROJECTS</span>
         </div>
         <div className="projects">
-          <Project title="wix-template" technologies={"Html, Css, JavaScript, React"} liveHref="http://127.0.0.1:5173/"/>
+          <Project title="wix-template" technologies="Html, Css, JavaScript, React" liveHref="http://127.0.0.1:5173/"/>
           <Project title="wix-template" liveHref="http://127.0.0.1:5173/"/>
           <Project title="wix-template" liveHref="http://127.0.0.1:5173/"/>
           <Project title="wix-template" liveHref="http://127.0.0.1:5173/" codeHref="http://127.0.0.1:5173/"/>
@@ -45,7 +60,7 @@ function App() {
         <div className="h2 ft-primary cnt-align">
           <span>CONTACT</span>
         </div>
-
+        <Contact />
       </div>
     </>
   )
--- src/forms/Contact.jsx
+++ src/forms/Contact.jsx
@@ -3,24 +3,22 @@ import '/src/styles/ContactForm.css';
 function Contact() {
     return(
         <div>
-            <form action="">
-                <div>
-                    <span>
-                        <label htmlFor="">First Name</label>
-                        <input type="text" name="" id="" />
-                    </span>
-                    <span>
-                        <label htmlFor="">Last Name</label>
-                        <input type="text" name="" id="" />
-                    </span>
+            <form className="contact-cnt" action="">
+                <div className="contact-fullname">
+                    <label className="block-dp" htmlFor="">First Name</label>
+                    <input className="block-dp" type="text" name="" id="" />
                 </div>
-                <div>
-                    <label htmlFor="">Subject</label>
-                    <input type="text" name="" id="" />
+                <div className="contact-email">
+                    <label className="block-dp" htmlFor="">Email</label>
+                    <input className="block-dp" type="email" name="" id="" />
                 </div>
-                <div>
-                    <label htmlFor="">Message</label>
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                <div className="contact-subject">
+                    <label className="block-dp" htmlFor="">Subject</label>
+                    <input className="block-dp" type="text" name="" id="" />
+                </div>
+                <div className="contact-message">
+                    <label className="block-dp" htmlFor="">Message</label>
+                    <textarea className="block-dp" name="" id="" cols="30" rows="10"></textarea>
                 </div>
             </form>
         </div>
